ff2d10e5f44128e5abd63e4b7261b24ae08e511c
Alan O'Donnell <alan.m.odonnell@gmail.com>
2017-06-02 14:25:06 -0400

    Add algolia dep.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import AlgoliaSearch from 'algoliasearch'
+
+const algoliaClient = new AlgoliaSearch('<app-id>', '<api-key>')
+const algoliaIndex = algoliaClient.initIndex('<index-name>')
+
+algoliaIndex.search('rick owens')
+  .then(console.log)
+  .catch(err => console.log('woops', err));
 
 ReactDOM.render(
   <p>Hi!</p>,
